72d51c8a00ece63f5ff4b067f88a4f096451c7fe
Blake Grudzien <blakegrudzien@gmail.com>
2024-01-22 14:03:20 -0500

    started react frontend

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  const headerStyle = {
+    backgroundColor: 'royalblue',
+    color: '#fff'
+  };
+  return (
+    <header style = {headerStyle}>
+      <h1>Play Hearts</h1>
+
+
+    </header>
+  )
+}
+
+export default Header
