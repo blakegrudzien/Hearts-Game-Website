6102dcd36efb3d49222ff117fa464196eeaacd61
Blake Grudzien <blakegrudzien@gmail.com>
2024-01-23 01:03:48 -0500

    Continued changes to front and backends

--- Hearts-Frontend/src/App.js
+++ Hearts-Frontend/src/App.js
@@ -1,18 +1,22 @@
-
-
+import React from 'react';
 import Header from './Header';
 import Content from './Content';
-import Footer from './Footer'
+import Footer from './Footer';
 
 function App() {
-  
- 
-  
+  const handleClick = () => {
+    fetch('/api/some-endpoint')
+  .then(response => response.text())
+  .then(text => console.log(text))
+  .catch(error => console.error(error));
+  };
+
   return (
     <div className="App">
       <Header />
       <Content />
-      <Footer/>
+      <Footer />
+      <button id="myButton" onClick={handleClick}>Click me!</button>
     </div>
   );
 }
