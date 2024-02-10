8240f0ce74ed61a23f62431e7a51c6233913329f
Blake Grudzien <blakegrudzien@gmail.com>
2024-02-10 12:52:14 -0500

    new Changes

--- Hearts-Frontend/src/config.js
+++ Hearts-Frontend/src/config.js
@@ -1,5 +1,6 @@
 // src/config.js
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+let API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+API_URL = API_URL.endsWith('/') ? API_URL.slice(0, -1) : API_URL;
 
 export default API_URL;
