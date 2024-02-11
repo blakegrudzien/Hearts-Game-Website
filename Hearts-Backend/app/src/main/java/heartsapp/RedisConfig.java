d0df6b19373ceb691b3a3792b5d5fda637b5ab39
Blake Grudzien <blakegrudzien@gmail.com>
2024-02-11 13:27:47 -0500

    further added TLS

--- Hearts-Backend/app/src/main/java/heartsapp/RedisConfig.java
+++ Hearts-Backend/app/src/main/java/heartsapp/RedisConfig.java
@@ -1,19 +1,13 @@
 package heartsapp;
 
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 
 public class RedisConfig {
 
@@ -33,17 +27,8 @@ public class RedisConfig {
             throw new IllegalArgumentException("REDIS_URL environment variable is not set");
         }
 
-        URI redisUri;
-        try {
-            redisUri = new URI(redisUrl);
-        } catch (URISyntaxException e) {
-            throw new IllegalArgumentException("Invalid REDIS_URL: " + e.getMessage(), e);
-        }
-
-        String password = null;
-        if (redisUri.getUserInfo() != null) {
-            password = redisUri.getUserInfo().split(":", 2)[1];
-        }
+        URI redisUri = URI.create(redisUrl);
+        String password = redisUri.getUserInfo().split(":", 2)[1];
 
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(10);
@@ -57,20 +42,7 @@ public class RedisConfig {
         SSLContext sslContext;
         try {
             sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[]{new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {
-                }
-
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            }}, null);
+            sslContext.init(null, null, null);
         } catch (NoSuchAlgorithmException | KeyManagementException e) {
             throw new RuntimeException("Failed to create SSL context", e);
         }
